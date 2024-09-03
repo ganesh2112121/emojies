855a16cf52f87b7f866b55238d471a2a19289cd9
Guguloth Ganesh <ganeshpagal558@gmail.com>
2024-09-03 23:23:36 +0530

    commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ export default function Nav(){
         <div className={styles.nav}>
             
             <div>
-                <img className={styles.img} width="78" height="78" src="https://img.icons8.com/external-flat-icons-vectorslab/68/external-Emojis-social-media-flat-icons-vectorslab.png" alt="external-Emojis-social-media-flat-icons-vectorslab"/> 
+                {/* <img className={styles.img} width="78" height="78" src="https://img.icons8.com/external-flat-icons-vectorslab/68/external-Emojis-social-media-flat-icons-vectorslab.png" alt="external-Emojis-social-media-flat-icons-vectorslab"/>  */}
                 <span className={styles.title}>Emo Trackr</span>
             </div>
         </div>
